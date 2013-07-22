cd3f77fe19c78044294cbe07287568288d14afd1
kvlung <kvlung@gmail.com>
2013-07-22 15:31:34 -0700

    Added index.html and web.js

---
+++ web.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+    response.send(fs.readFileSync(htmlfile).toString());
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
