f6ca9556c7f0fdf192461958cdc8208e27e8808e
Kevin Lung <kvlung@gmail.com>
2013-09-01 18:42:21 -0700

    Updated web.js

--- web.js
+++ web.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var fs = require('fs');
 var htmlfile = "index.html";
+var htmlfileabout = "about.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     response.send(fs.readFileSync(htmlfile).toString());
+});
+
+app.get('/about.html',function(requestion,response){
+    response.send(fs.readFileSync(htmlfileabout).toString());
 });
 
 var port = process.env.PORT || 8080;
